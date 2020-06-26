8539c3eacbc78907ac2a47b3fc23c51bd7a9d521
Daniel Grychtol <grychtol.daniel@gmail.com>
2020-06-26 08:13:41 +0200

    Rename spring varible

--- src/AnimatedScreen.tsx
+++ src/AnimatedScreen.tsx
@@ -114,7 +114,13 @@ const AnimatedScreen = () => {
   const [dragState] = useState(new Value(0));
   const [clock] = useState(new Clock());
 
-  const spring = runSpring(dragY, dragCompensator, velocity, clock, dragState);
+  const animation = runSpring(
+    dragY,
+    dragCompensator,
+    velocity,
+    clock,
+    dragState,
+  );
 
   const dragHandler = event([
     {
@@ -141,7 +147,10 @@ const AnimatedScreen = () => {
         onHandlerStateChange={dragHandler}>
         <Animated.View style={styles.scrollBox}>
           <Animated.View
-            style={[styles.primaryScreen, {transform: [{translateY: spring}]}]}>
+            style={[
+              styles.primaryScreen,
+              {transform: [{translateY: animation}]},
+            ]}>
             <Title>{"Hello there!\nIt's me, animation!"}</Title>
 
             <CustomText>
